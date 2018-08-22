efcae933f095cceb35cf09d657e8cc1cd1f06fae
anushajagarlamudi <41891193+anushajagarlamudi@users.noreply.github.com>
2018-08-22 12:50:16 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		for(int i=n+1;i<q;i++)
+		if(i%2==0)
+		System.out.println(i);
+	}
+}
